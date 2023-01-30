1537eb83ee9339cfc8b1bb31b3200a60df3e765b
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-01-30 23:30:17 +0400

    add images

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 let json = `[{
     "title": "Stark",
+    "image": "img/Stark.jpg",
     "character": "Iron Man",
     "name": "Tony Stark",
     "superpowers": "intelligence"
 },{
     "title": "Cap",
+    "image": "img/Cap.jpg",
     "character": "Captain America",
     "name": "Stephen Rogers",
-    "superpowers": "strength, endurance, regeneration"
+    "superpowers": "strength,<br> endurance,<br> regeneration"
 },{
     "title": "Natasha",
+    "image": "img/Natasha.jpg",
     "character": "Black Widow",
     "name": "Natasha Romanoff",
     "superpowers": "spy"
 },{
     "title": "Thor",
+    "image": "img/Thor.jpg",
     "character": "Thor",
     "name": "Thor Odinson",
-    "superpowers": "the power of a god, the hammer Mjolnir"
+    "superpowers": "the power of a god, <br> the hammer Mjolnir"
 },{
     "title": "Hawkeye",
+    "image": "img/Hawkeye.jpg",
     "character": "Hawkeye",
     "name": "Clint Barton",
-    "superpowers": "great shooter"
+    "superpowers": "great shooter,<br> Natasha's best friend"
 },{
     "title": "Banner",
+    "image": "img/Benner.jpg",
     "character": "Hulk",
     "name": "Bruce Banner",
-    "superpowers": "intelligence, superhuman strength, flight"
+    "superpowers": "intelligence,<br> superhuman strength,<br> flight"
 }]`;
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -37,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     for (let card of cards) {
         cardsContent += `<div class='card'>
         <h2>${card.title}</h2>
-        <img src="${card.image}">
+        <img src="${card.image}" style="width:250px" />
         <div>${card.character}</div>
         <div>${card.name}</div>
         <div>${card.superpowers}</div>
